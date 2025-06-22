a17e1160b699221d88a1305abb114c4c04b357e3
D240021 <danielbarrantesq@gmail.com>
2025-06-21 18:22:43 -0600

    Se configura vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  base: mode === 'production' ? '/Turrialba-Emprende/' : '/',
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist'
+    },
+    base: isProduction ? '/Turrialba-Emprende/' : '/',
+  }
 })
